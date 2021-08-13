c636089cd4094510d7bda807faa8ffbb1e144804
John Gartsu <jgartsu12@gmail.com>
2021-08-13 10:22:42 -0400

    debugged error: MissingRequiredParameter: Missing required key 'Item' in params

--- putUserData/index.js
+++ putUserData/index.js
@@ -4,11 +4,12 @@
 const AWS = require('aws-sdk');
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-
 exports.handler = async (event, context) =>  {
     const params = {
         TableName: "Users",
-        Key: {
+        //debugged error: MissingRequiredParameter: Missing required key 'Item' in params (needed to be Item not Key sense we are Putting/Adding item)
+        // when excuted, because I didnt change the id to an unique identifier it changed the first & last name of that id from Bob Jone to Jay Lewis
+        Item: {
             id: "12345",
             firstname: "Jay",
             lastname: "Lewis"     
