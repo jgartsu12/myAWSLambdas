b9a5c066ad5faa73dec59f964f93d7ad61194b4f
John Gartsu <jgartsu12@gmail.com>
2021-08-13 10:29:03 -0400

    changing id worked to add new items

--- putUserData/index.js
+++ putUserData/index.js
@@ -9,10 +9,13 @@ exports.handler = async (event, context) =>  {
         TableName: "Users",
         //debugged error: MissingRequiredParameter: Missing required key 'Item' in params (needed to be Item not Key sense we are Putting/Adding item)
         // when excuted, because I didnt change the id to an unique identifier it changed the first & last name of that id from Bob Jone to Jay Lewis
+       // changing the id worked and added new item
         Item: {
             id: "12345",
             firstname: "Jay",
-            lastname: "Lewis"     
+            lastname: "Lewis"    
+                    // in console looks like {} and null because not returning a value
+ 
         }
     }
     try {
