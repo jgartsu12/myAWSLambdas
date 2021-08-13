0c1832a453142e748aabac1e8cd3a6cc5931e46c
John Gartsu <jgartsu12@gmail.com>
2021-08-13 09:57:31 -0400

    debugged error:  AWS.DynamoDB.documentClient is not a constructor

--- getUserData/getUserDataV2.js
+++ getUserData/getUserDataV2.js
@@ -1,5 +1,34 @@
 // use documentclient so you dont have to specify the data type for the dynamoDB Json, unmarshall our ddb json to standard json
+//how to use modern client now without getting error: type error: AWS.DynamoDB.documentClient is not a constructor
+'use strict'
+const AWS = require('aws-sdk');
+// const documentClient = new AWS.DynamoDB.documentClient();
+var docClient = new AWS.DynamoDB.DocumentClient();
 
+// AWS.config.update({ region: "us-east-1" });
+
+
+exports.handler = function (event, context, callback) {
+    const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+    // const documentClient = new AWS.DynamoDB.documentClient({ region: "us-east-1" });
+    const params = {
+        TableName: "Users",
+        Key: {
+            id: "12345"     
+        }
+    }
+    // with docuclient syntax is just get not getItem()
+    docClient.get(params, (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(data);
+    });
+}
+
+
+/*
+'deprecated methods below'
 'use strict'
 const AWS = require('aws-sdk');
 AWS.config.update({ region: "us-east-1" });
@@ -22,3 +51,4 @@ exports.handler = function (event, context, callback) {
         console.log(data);
     });
 }
+*/
